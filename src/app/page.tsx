6586ff0eb10e5276978fe50dfb428f5b79f4acff
Marcin <marcinkukulka@gmail.com>
2023-10-20 11:10:10 +0200

    feat: add stripe payment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
+import Stripe from 'stripe';
+async function handleStripePaymentAction() {
+		'use server'
+		if(!process.env.STRIPE_SECRET_KEY) {
+			throw new Error('Missing Stripe secret key env variable');
+		}
+
+		const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+			apiVersion: '2023-10-16',
+			typescript: true,
+		});
+		}
+		function handleExternalMessage(event: MessageEvent) {
+			// Check the origin of the message
+			if (event.origin !== 'https://example.com') {
+				return;
+			}
+
+			// Handle the message data
+			const data = event.data;
+			console.log('Received message:', data);
+		}
+
+		// Listen for messages from the external page
+		window.addEventListener('message', handleExternalMessage);
+	
 export default function Home() {
-	return <main></main>;
+	
+	return (
+		<main>
+			<form action={handleStripePaymentAction} className="ml-auto">
+				<button
+					type="submit"
+					className="rounded-sm border bg-slate-100 px-8 py-2 shadow-sm transition-colors hover:bg-slate-200"
+				>
+					Pay
+				</button>
+			</form>
+		</main>
+	);
 }
