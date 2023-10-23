89ea366eeb538a9684f73469406b6678e3a67483
Marcin <marcinkukulka@gmail.com>
2023-10-23 22:21:43 +0200

    bugfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,27 @@
 import Stripe from 'stripe';
 async function handleStripePaymentAction() {
-		'use server'
-		if(!process.env.STRIPE_SECRET_KEY) {
-			throw new Error('Missing Stripe secret key env variable');
-		}
+	'use server';
+	if (!process.env.STRIPE_SECRET_KEY) {
+		throw new Error('Missing Stripe secret key env variable');
+	}
 
-		const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-			apiVersion: '2023-10-16',
-			typescript: true,
-		});
-		}
-		function handleExternalMessage(event: MessageEvent) {
-			if (event.origin !== 'https://example.com') {
-				return;
-			}
+	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+		apiVersion: '2023-10-16',
+		typescript: true,
+	});
+}
+function handleExternalMessage(event: MessageEvent) {
+	if (event.origin !== 'https://example.com') {
+		return;
+	}
 
-			
-			const data = event.data;
-			console.log('Received message:', data);
-		}
+	const data = event.data;
+	console.log('Received message:', data);
+}
 
-		window.addEventListener('message', handleExternalMessage);
-	
+window.addEventListener('message', handleExternalMessage);
+
 export default function Home() {
-	
 	return (
 		<main>
 			<form action={handleStripePaymentAction} className="ml-auto">
